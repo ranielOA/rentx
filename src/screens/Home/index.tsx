a32d984fa7bf4be69a7251eba86f82d4b7467e3d
ranielOA <r.a_tw@hotmail.com>
2021-09-01 23:53:48 -0300

    card car creation

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,10 +3,21 @@ import { StatusBar } from 'react-native';
 import { RFValue } from 'react-native-responsive-fontsize';
 
 import Logo from '../../assets/logo.svg';
+import { Car } from '../../components/Car';
 
 import { Container, Header, TotalCars, HeaderContent } from './styles';
 
 export function Home() {
+  const carData = {
+    brand: 'Audi',
+    name: 'RS 5 Coupé',
+    rent: {
+      period: 'AO DIA',
+      price: 120,
+    },
+    thumbnail:
+      'https://e7.pngegg.com/pngimages/140/866/png-clipart-hyundai-creta-car-mini-sport-utility-vehicle-hyundai-compact-car-car.png',
+  };
   return (
     <Container>
       <StatusBar
@@ -20,6 +31,8 @@ export function Home() {
           <TotalCars>Total de 12 carros</TotalCars>
         </HeaderContent>
       </Header>
+
+      <Car data={carData} />
     </Container>
   );
 }

2fe77ba35eb537fa7086adcede333302e1b8604c
ranielOA <r.a_tw@hotmail.com>
2021-09-02 22:23:36 -0300

    created slider component

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { BackButton } from '../../components/BackButton';
+import { ImageSlider } from '../../components/ImageSlider';
 
-import { Container, Header } from './styles';
+import { Container, Header, CarImages } from './styles';
 
 export function CarDetails() {
   return (
@@ -9,6 +10,14 @@ export function CarDetails() {
       <Header>
         <BackButton onPress={() => {}} />
       </Header>
+
+      <CarImages>
+        <ImageSlider
+          imagesUrl={[
+            'https://e7.pngegg.com/pngimages/140/866/png-clipart-hyundai-creta-car-mini-sport-utility-vehicle-hyundai-compact-car-car.png',
+          ]}
+        />
+      </CarImages>
     </Container>
   );
 }

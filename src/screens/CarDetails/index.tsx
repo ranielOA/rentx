72502e17c395aae709b340025799d2d3a4e00663
ranielOA <r.a_tw@hotmail.com>
2021-09-02 22:37:25 -0300

    details interface

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -2,7 +2,20 @@ import React from 'react';
 import { BackButton } from '../../components/BackButton';
 import { ImageSlider } from '../../components/ImageSlider';
 
-import { Container, Header, CarImages } from './styles';
+import {
+  Container,
+  Header,
+  CarImages,
+  Description,
+  Rent,
+  Content,
+  Details,
+  Brand,
+  Name,
+  Period,
+  Price,
+  About,
+} from './styles';
 
 export function CarDetails() {
   return (
@@ -18,6 +31,25 @@ export function CarDetails() {
           ]}
         />
       </CarImages>
+
+      <Content>
+        <Details>
+          <Description>
+            <Brand>Lamborghini</Brand>
+            <Name>Huracan</Name>
+
+            <Rent>
+              <Period>Ao dia</Period>
+              <Price>R$ 580</Price>
+            </Rent>
+          </Description>
+        </Details>
+
+        <About>
+          Este é automóvel desportivo. Surgiu do lendário touro de lide
+          indultado na praça Real Maestranza de Sevilla.
+        </About>
+      </Content>
     </Container>
   );
 }

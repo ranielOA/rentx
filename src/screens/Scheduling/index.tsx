6c9f4c697bd16ffe2502cc185c3c7279f10cb6bf
ranielOA <r.a_tw@hotmail.com>
2021-09-06 01:00:13 -0300

    added calendar

--- src/screens/Scheduling/index.tsx
+++ src/screens/Scheduling/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
 import { useTheme } from 'styled-components';
+
 import { BackButton } from '../../components/BackButton';
+import { Button } from '../../components/Button';
+import { Calendar } from '../../components/Calendar';
 
 import ArrowSvg from '../../assets/arrow.svg';
 
@@ -16,7 +19,6 @@ import {
   Content,
   Footer,
 } from './styles';
-import { Button } from '../../components/Button';
 
 export function Scheduling() {
   const theme = useTheme();
@@ -53,7 +55,9 @@ export function Scheduling() {
         </RentalPeriod>
       </Header>
 
-      <Content></Content>
+      <Content>
+        <Calendar />
+      </Content>
 
       <Footer>
         <Button title="Confirmar" />

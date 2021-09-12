14e71df322080d806b2f8b3d61f31b31c16fde4e
ranielOA <r.a_tw@hotmail.com>
2021-09-11 22:48:46 -0300

    listing user schedules

--- src/dtos/SchedulesDTO.ts
+++ src/dtos/SchedulesDTO.ts
@@ -1,4 +1,14 @@
+import { CarDTO } from './CarDTO';
+
 export interface SchedulesByCarDTO {
   id: string;
   unavailable_dates: string[];
+}
+
+export interface SchedulesByUserDTO {
+  user_id: number;
+  car: CarDTO;
+  startDate: string;
+  endDate: string;
+  id: number;
 }
--- src/screens/MyCars/index.tsx
+++ src/screens/MyCars/index.tsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { CarDTO } from '../../dtos/CarDTO';
+import { SchedulesByUserDTO } from '../../dtos/SchedulesDTO';
+import { api } from '../../services/api';
 
 import { Container } from './styles';
 
 export function MyCars() {
+  const [cars, setCars] = useState<CarDTO[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchCars() {
+      try {
+        const response = await api.get('schedules_byuser?user_id=1');
+        const schedulesByUser: SchedulesByUserDTO[] = response.data;
+
+        setCars(schedulesByUser.map((sc) => sc.car));
+      } catch (error) {}
+    }
+
+    fetchCars();
+  }, []);
+
   return <Container></Container>;
 }

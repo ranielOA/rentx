3561e6ca808956df51d329de44a1b758c629df44
ranielOA <r.a_tw@hotmail.com>
2021-10-07 23:49:28 -0300

    password input

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -3,6 +3,7 @@ import { StatusBar } from 'react-native';
 import { useTheme } from 'styled-components';
 import { Button } from '../../components/Button';
 import { Input } from '../../components/Input';
+import { PasswordInput } from '../../components/PasswordInput';
 
 import {
   Container,
@@ -43,6 +44,8 @@ export function SignIn() {
         autoCorrect={false}
         autoCapitalize="none"
       />
+
+      <PasswordInput iconName="lock" placeholder="Senha" />
 
       <Footer>
         <Button
